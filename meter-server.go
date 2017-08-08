43e6800fd71a617ad9e634a45c9ef47d6c0f28b1
Jory Geerts <jory@jorygeerts.nl>
2017-08-08 20:58:45 +0200

    Implement measurement detail

--- meter-server.go
+++ meter-server.go
@@ -8,13 +8,30 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"regexp"
 	"flag"
-	"fmt"
 	"strings"
 	"time"
+	"errors"
+	"strconv"
 )
 
 type Meter struct {
 	db *sql.DB
+}
+
+func (meter *Meter) getCollection(id int) (*Collection, error)  {
+	rows, err := meter.db.Query("SELECT id, name, icon FROM collection WHERE id = ? LIMIT 1", id);
+
+	checkErr(err);
+
+	defer rows.Close();
+
+	if rows.Next() == false {
+		return nil, errors.New("No collection with id " + string(id));
+	}
+	collection := new(Collection);
+	rows.Scan(&collection.Id, &collection.Name, &collection.Icon);
+
+	return collection, nil;
 }
 
 func (meter *Meter) collectionListHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,20 +60,19 @@ func (meter *Meter) collectionListHandler(w http.ResponseWriter, r *http.Request
 func (meter *Meter) collectionDetailHandler(w http.ResponseWriter, r *http.Request) {
 
 	parts := strings.Split(r.URL.Path, "/");
-	id := parts[1];
+	id, err := strconv.Atoi(parts[1]);
 
-	rows, err := meter.db.Query("SELECT id, name, icon FROM collection WHERE id = ? LIMIT 1", id);
-
-	checkErr(err);
-
-	defer rows.Close();
-
-	if rows.Next() == false {
+	if err != nil {
 		http.NotFound(w, r);
 		return;
 	}
-	collection := new(Collection);
-	rows.Scan(&collection.Id, &collection.Name, &collection.Icon);
+
+	collection, err := meter.getCollection(id);
+
+	if err != nil {
+		http.NotFound(w, r);
+		return;
+	}
 
 	mRows, err := meter.db.Query("SELECT id, measured_value, measured_at FROM measurement WHERE collection_id = ? ORDER BY measured_at DESC", collection.Id);
 
@@ -81,10 +97,48 @@ func (meter *Meter) collectionDetailHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (meter *Meter) measurementDetailHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL: %s\n", r.URL.Path[1:]);
-	fmt.Fprintf(w, "URL: %s\n", r.URL.Path[1:1]);
-	fmt.Fprintf(w, "URL: %s\n", r.URL.Path[1:2]);
-	fmt.Fprintf(w, "URL: %s\n", r.URL.Path[3:7]);
+
+	collectionId, err := strconv.Atoi(r.URL.Path[1:2]);
+	year := r.URL.Path[3:7];
+	month := r.URL.Path[8:10];
+	day := r.URL.Path[11:13];
+
+	if err != nil {
+		http.NotFound(w, r);
+		return;
+	}
+
+	collection, err := meter.getCollection(collectionId);
+
+	if err != nil {
+		http.NotFound(w, r);
+		return;
+	}
+
+	dash := string('-');
+	percent := string('%');
+	date := year + dash + month + dash + day;
+
+	rows, err := meter.db.Query("SELECT id, measured_value, measured_at FROM measurement WHERE collection_id = ? AND measured_at LIKE ?", collection.Id, percent + date + percent);
+
+	checkErr(err);
+
+	defer rows.Close();
+
+	if rows.Next() == false {
+		http.NotFound(w, r);
+		return;
+	}
+
+	var measureTime []byte
+	measurement := new(Measurement);
+	measurement.Collection = *collection;
+
+	rows.Scan(&measurement.Id, &measurement.Value, &measureTime);
+	measurement.Date, _ = time.Parse("2006-01-02 15:04:00", string(measureTime));
+
+	w.Header().Add("Content-type", "application/json");
+	json.NewEncoder(w).Encode(measurement);
 }
 
 func checkErr(err error) {
